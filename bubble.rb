344546ab25dcbeba3943f1542b6ebf2ddf8fe7bc
Cosmel Villalobos <cosmelvg@gmail.com>
2020-10-09 11:54:04 -0400

    working on the rubocop errors

--- bubble.rb
+++ bubble.rb
@@ -1,62 +1,46 @@
 first_array = [5, 9, -3, -2, 4, 1]
 
- print first_array
+print first_array
 
- def bubble_sort(arr)
-   k = arr.length
-   while k > 0
-     n = 0
-     while n < arr.length
-         if arr[n + 1] != nil && arr[n] > arr[n + 1]
-             arr[n], arr[n + 1] = arr[n + 1], arr[n]
-         end
-         n += 1
-     end
-     k -= 1
-   end
-   print arr
- end
+def bubble_sort(arr)
+  k = arr.length
+  while k.positive?
+    n = 0
+    while n < k
+      arr[n], arr[n + 1] = arr[n + 1], arr[n] if !arr[n + 1].nil? && arr[n] > arr[n + 1]
+      n += 1
+    end
+    k -= 1
+  end
+  print arr
+end
 
- bubble_sort(first_array)
+bubble_sort(first_array)
 
-
-
-
-begin
 second_array = %w[hi hello hey]
 
 def bubble_sort_by(arr)
-    n = 0
-    while n < arr.length
-        if  arr[n + 1] != nil && (arr[n].length - arr[n + 1].length) > 0 
-            arr[n], arr[n + 1] = arr[n + 1], arr[n]
-        end
-        n += 1
-    end
-    print arr
+  n = 0
+  while n < arr.length
+    arr[n], arr[n + 1] = arr[n + 1], arr[n] if !arr[n + 1].nil? && (arr[n].length - arr[n + 1].length).positive?
+    n += 1
+  end
+  print arr
 end
 
 bubble_sort_by(second_array)
 
-
-
-
 second_array = %w[hi hello hey]
 
 def one_yield()
-    yield
+  yield
 end
 
-one_yield() { puts second_array }
+one_yield { puts second_array }
 
-
-
-
-
-
-
-
-
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
 
 #     n = 0
 #     if r > 0
@@ -64,4 +48,3 @@ one_yield() { puts second_array }
 #         arr[n], arr[n + 1] = arr[n + 1], arr[n]
 #         # result positive means left smaller then do nothing
 #         # result 0 means both are equal then do nothing
-#     end
