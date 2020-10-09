53fcb2808ccb1783cd18ce463f66ffa7ff3b9491
alan soto valle <soto.alan@hotmail.com>
2020-10-09 10:52:31 -0500

    Adding yield  to second par of the code

--- bubble.rb
+++ bubble.rb
@@ -1,3 +1,4 @@
+=begin
 first_array = [5, 9, -3, -2, 4, 1]
 
  print first_array
@@ -17,51 +18,31 @@ first_array = [5, 9, -3, -2, 4, 1]
    print arr
  end
 
- bubble_sort(first_array)
+bubble_sort(first_array)
+=end
 
-
-
-
-begin
-second_array = %w[hi hello hey]
+second_array = %w[heelloo hi hello hey h]
 
 def bubble_sort_by(arr)
-    n = 0
-    while n < arr.length
-        if  arr[n + 1] != nil && (arr[n].length - arr[n + 1].length) > 0 
-            arr[n], arr[n + 1] = arr[n + 1], arr[n]
+    k = arr.length
+    while k > 0
+        n = 0
+        while n < arr.length
+            if  arr[n + 1] != nil && yield(arr[n], arr[n + 1]) > 0 
+                arr[n], arr[n + 1] = arr[n + 1], arr[n]
+            end
+            n += 1
         end
-        n += 1
+        k -= 1
     end
     print arr
 end
 
-bubble_sort_by(second_array)
-
-
-
-
-second_array = %w[hi hello hey]
-
-def one_yield()
-    yield
+bubble_sort_by(second_array) do |a, b|
+    a.length - b.length
 end
 
-one_yield() { puts second_array }
 
 
 
 
-
-
-
-
-
-
-#     n = 0
-#     if r > 0
-#         # result negative means left greater then swap
-#         arr[n], arr[n + 1] = arr[n + 1], arr[n]
-#         # result positive means left smaller then do nothing
-#         # result 0 means both are equal then do nothing
-#     end
